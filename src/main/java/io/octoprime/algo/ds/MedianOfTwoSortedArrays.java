0506eec7169754d4240c67b3ece2b1b1f7132966
Jeffrey T. Lucas <lucasjt@gmail.com>
2018-08-08 13:10:41 -0700

    Added new implementation of finding median of two sorted arrays with varying lengths.  Should be able to handle length of odd & even.

---
+++ src/main/java/io/octoprime/algo/ds/MedianOfTwoSortedArrays.java
@@ -0,0 +1,47 @@
+package io.octoprime.algo.ds;
+
+public class MedianOfTwoSortedArrays {
+
+
+    public static void main(String[] args) {
+        int[] x = {1, 3, 8, 9, 15};
+        int[] y = {7, 11, 19, 21, 18, 25};
+
+        double val = (new MedianOfTwoSortedArrays()).findMedianSortedArray(x, y);
+        System.out.println(String.format("The mediam of two sorted array with varying length is: %1$,.2f", val));
+    }
+
+    public double findMedianSortedArray(int[] input1, int[] input2) {
+
+        if (input1.length > input2.length) {
+            return findMedianSortedArray(input2, input1);
+        }
+
+        int x = input1.length;
+        int y = input2.length;
+
+        int low = 0;
+        int high = x;
+
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
+
+            int maxLeftX = partitionX == 0 ? Integer.MIN_VALUE : input1[partitionX - 1];
+            int minRightX = partitionX == x ? Integer.MAX_VALUE : input1[partitionX];
+
+            int maxLeftY = partitionY == 0 ? Integer.MIN_VALUE : input2[partitionY - 1];
+            int minRightY = partitionY == 0 ? Integer.MAX_VALUE : input2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if (((x + y) % 2) == 0) {
+                    return ((double) Math.max(maxLeftY, maxLeftX) + Math.min(minRightX, minRightY)) / 2;
+                } else return (double) Math.max(maxLeftX, maxLeftY);
+            } else if (maxLeftX > minRightY) high = partitionX - 1;
+            else low = partitionX + 1;
+
+        }
+
+        throw new IllegalArgumentException();
+    }
+}

0ccdbdfe13926395d70dd8f77fdcc070c4af78bf
Jeffrey T. Lucas <lucasjt@gmail.com>
2018-08-10 21:35:26 -0700

    Moved sumOfDigitsTarget to math package.

--- src/main/java/io/octoprime/algo/Practice.java
+++ src/main/java/io/octoprime/algo/Practice.java
@@ -2,8 +2,6 @@ package io.octoprime.algo;
 
 import io.octoprime.algo.strings.UtilsString;
 
-import java.util.Arrays;
-
 public class Practice {
 
 
@@ -16,27 +14,6 @@ public class Practice {
 
     }
 
-    public static void easy2SumOfTarget(int target) {
-        int arr[] = new int[]{2, 2, 7, 3, 12};
-        int t[] = {};
-
-        int tar = 9;
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                int sum = arr[i] + arr[j];
-                if (sum == target) {
-                    t = new int[2];
-                    t[0] = i;
-                    t[1] = j;
-                    break;
-                }
-            }
-        }
-
-        System.out.println(Arrays.toString(t));
-
-    }
 
     public static void main(String[] args) {
         int x = 1028;
@@ -44,7 +21,7 @@ public class Practice {
         System.out.println("Value is :" + x);
         // easy1ReverseString();
 
-        boolean isPowerOfTwo = (boolean) ((x & (x - 1)) == 0);
+        boolean isPowerOfTwo = ((x & (x - 1)) == 0);
         System.out.println("Is Power of 2: " + (isPowerOfTwo ? "true" : "false"));
 
         //  easy2SumOfTarget(5);
--- src/main/java/io/octoprime/algo/math/SumDigits.java
+++ src/main/java/io/octoprime/algo/math/SumDigits.java
@@ -1,5 +1,6 @@
 package io.octoprime.algo.math;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SumDigits {
@@ -26,6 +27,28 @@ public class SumDigits {
             ;
 
         return sum;
+    }
+
+    public static void easy2SumOfTarget(int target) {
+        int arr[] = new int[]{2, 2, 7, 3, 12};
+        int t[] = {};
+
+        int tar = 9;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int sum = arr[i] + arr[j];
+                if (sum == target) {
+                    t = new int[2];
+                    t[0] = i;
+                    t[1] = j;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(t));
+
     }
 
     public static void main(String[] args) {

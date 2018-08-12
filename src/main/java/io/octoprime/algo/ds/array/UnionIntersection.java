92b20d3c7a83776aa99c2c8e6e1b06dd55979090
Jeffrey T. Lucas <lucasjt@gmail.com>
2018-08-11 17:21:22 -0700

    Changed implementation of union & intersection to return lists.

--- src/main/java/io/octoprime/algo/ds/array/UnionIntersection.java
+++ src/main/java/io/octoprime/algo/ds/array/UnionIntersection.java
@@ -1,9 +1,10 @@
 package io.octoprime.algo.ds.array;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
- *
+ * Utility class for generation union and intersection of two arrays.
  */
 public class UnionIntersection {
 
@@ -12,15 +13,19 @@ public class UnionIntersection {
     /**
      * Prints union of arr1[0..m-1] and arr2[0..n-1]
      *
-     * @param arr1
-     * @param arr2
-     * @param m
-     * @param n
+     * @param arr1 first array
+     * @param arr2 second array
      */
-    void union(int arr1[], int arr2[], int m, int n) {
+    int[] union(int arr1[], int arr2[]) {
 
-        // Before finding union, make sure arr1[0..m-1]
-        // is smaller
+        LinkedList<Integer> list = new LinkedList<>();
+
+        int m = arr1.length;
+        int n = arr2.length;
+
+        /*
+         * Before finding union, make sure arr1[0..m-1] is smaller
+         */
         if (m > n) {
             int tempp[] = arr1;
             arr1 = arr2;
@@ -31,32 +36,37 @@ public class UnionIntersection {
             n = temp;
         }
 
-        // Now arr1[] is smaller
-        // Sort the first array and print its elements (these two
-        // steps can be swapped as order in output is not important)
+        /**
+         *  Now arr1[] is smaller Sort the first array and print its elements (these two
+         *  steps can be swapped as order in output is not important)
+         */
         Arrays.sort(arr1);
         for (int i = 0; i < m; i++)
-            System.out.print(arr1[i] + " ");
+            list.add(arr1[i]);
 
-        // Search every element of bigger array in smaller array
-        // and print the element if not found
+        /**
+         * Search every element of bigger array in smaller array and add the element if not found
+         */
         for (int i = 0; i < n; i++) {
             if (_bs.bsearch(arr1, 0, m - 1, arr2[i]) == -1)
-                System.out.print(arr2[i] + " ");
+                list.add(arr2[i]);
         }
+
+        return list.stream().mapToInt(e -> e).toArray();
     }
 
     /**
      * Prints intersection of arr1[0..m-1] and arr2[0..n-1]
      *
-     * @param arr1
-     * @param arr2
-     * @param m
-     * @param n
+     * @param arr1 first array
+     * @param arr2 second array
      */
-    void intersection(int arr1[], int arr2[], int m, int n) {
+    int[] intersection(int arr1[], int arr2[]) {
         // Before finding intersection, make sure arr1[0..m-1]
         // is smaller
+        int m = arr1.length;
+        int n = arr2.length;
+
         if (m > n) {
             int tempp[] = arr1;
             arr1 = arr2;
@@ -73,20 +83,26 @@ public class UnionIntersection {
 
         // Search every element of bigger array in smaller array
         // and print the element if found
+        LinkedList<Integer> list = new LinkedList();
+
         for (int i = 0; i < n; i++) {
             if (_bs.bsearch(arr1, 0, m - 1, arr2[i]) != -1)
-                System.out.print(arr2[i] + " ");
+                list.add(arr2[i]);
         }
+
+        return list.stream().mapToInt(i -> i).toArray();
     }
 
     /**
      * Driver program to test above functions
-     * @param args
+     *
+     * @param args standard arguments for CLI
      */
     public static void main(String[] args) {
         UnionIntersection ui = new UnionIntersection();
         int arr1[] = {7, 1, 5, 2, 3, 6};
         int arr2[] = {3, 8, 6, 20, 7};
+        int[] list;
 
         System.out.println("Array-1: " + Arrays.toString((arr1)));
         System.out.println("Array-2: " + Arrays.toString((arr2)));
@@ -95,14 +111,15 @@ public class UnionIntersection {
         /*
          * union of two arrays
          */
-        System.out.println("Union of two arrays is ");
-        ui.union(arr1, arr2, arr1.length, arr2.length);
-        System.out.println();
+        System.out.print("Union of two arrays is: ");
+        list = ui.union(arr1, arr2);
+        System.out.println(Arrays.toString(list));
 
         /*
          * intersection of two arrays.
          */
-        System.out.println("Intersection of two arrays is ");
-        ui.intersection(arr1, arr2, arr1.length, arr2.length);
+        list = ui.intersection(arr1, arr2);
+        System.out.print("Intersection of two arrays is: ");
+        System.out.println(Arrays.toString(list));
     }
 }

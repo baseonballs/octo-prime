20e97680c634dfd512deca16a9b33c516aba6fd0
Jeffrey T. Lucas <lucasjt@gmail.com>
2018-08-19 16:28:45 -0700

    added a missing file for the preorder implementation of bst.

--- src/main/java/io/octoprime/algo/sort/Sort.java
+++ src/main/java/io/octoprime/algo/sort/Sort.java
@@ -1,11 +1,12 @@
 package io.octoprime.algo.sort;
 
-import java.util.Arrays;
-
 public interface Sort {
 
     default void sort(Integer[] arr) {
-        sort(Arrays.stream(arr).mapToInt(x -> x).toArray());
+
+        throw new IllegalArgumentException();
+
+        // sort( Arrays.stream(arr).mapToInt(x -> x).toArray());
     }
 
     void sort(int[] arr);

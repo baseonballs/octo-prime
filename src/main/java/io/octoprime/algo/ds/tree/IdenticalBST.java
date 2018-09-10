3c13e8654262c6c9ba92e335d0addb90f17ff351
Jeffrey T. Lucas <lucasjt@gmail.com>
2018-09-10 15:00:51 -0700

    Removed inappropriate comments for the isIdenticalBinaryTree.

--- src/main/java/io/octoprime/algo/ds/tree/IdenticalBST.java
+++ src/main/java/io/octoprime/algo/ds/tree/IdenticalBST.java
@@ -1,16 +1,5 @@
 package io.octoprime.algo.ds.tree;
 
-/**
- * Problem
- * Compare two strings A and B, determine whether A contains all of the characters
- * in B.
- * The characters in string A and B are all Upper Case letters.
- * Example
- * For A = "ABCD" , B = "ACD" , return true.
- * For A = "ABCD" , B = "AABC" , return false.
- * Note
- * The characters of B in A are not necessary continuous or ordered.
- */
 public class IdenticalBST {
 
     /**
